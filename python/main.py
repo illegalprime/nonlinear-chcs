04247896bb89940992520e0a68e80910b3485236
Michael Eden <themichaeleden@gmail.com>
2018-07-21 22:12:27 -0400

    Finish decision tree refactor

--- python/main.py
+++ python/main.py
@@ -2,130 +2,144 @@ from subprocess import run, PIPE
 from sexpdata import loads
 from sklearn import tree
 from sklearn import svm
+import logging as logger
 import re
 import graphviz
 import decimal
+import os
 
 
-def simple_loop(*, x, y):
-    return {
-        'x': x + y,
-        'y': y + 1,
-    }
+class Relation:
+    def setup(self):
+        self.syms = list(sorted(self.start.keys()))
+        self.primer = re.compile('(%s)' % '|'.join(map(re.escape, self.syms)))
 
 
-relations = {
-    'simple': {
-        'head': {
-            'start': {
-                'x': 1,
-                'y': 0,
-            },
-            'logic': '(and (= x 1) (= y 0))',
-        },
-        'loop': {
-            'script': simple_loop,
-            'logic': """
-                (declare-const x Int)
-                (declare-const y Int)
-                (declare-const xp Int)
-                (declare-const yp Int)
+    def logic(self, expr):
+        hypothesis = self.to_hypo(expr)
+        def mk_declare(sym):
+            return """
+            (declare-const {0} Int)
+            (declare-const {0}p Int)
+            """.format(sym)
+        return """
+        {declare}
 
-                (assert {prerel})
-                (assert (= xp (+ x y)))
-                (assert (= yp (+ y 1)))
-                (assert (not {postrel}))
+        (assert {prerel})
+        {asserts}
+        (assert (not {postrel}))
 
-                (check-sat)
-                (get-model)
-            """,
-        },
-    },
-}
+        (check-sat)
+        (get-model)
+        """.format(**{
+            'declare': '\n'.join(map(mk_declare, self.syms)),
+            'asserts': '\n'.join(map(lambda a: '(assert %s)' % a, self.asserts)),
+            'prerel': hypothesis['prerel'],
+            'postrel': hypothesis['postrel'],
+        })
 
-
-def positive_points(relation):
-    current = relation['head']['start']
-    while True:
-        yield current
-        current = relation['loop']['script'](**current)
+    def positive_points(self):
+        current = self.start
+        while True:
+            yield current
+            current = self.script(**current)
 
 
-def negative_point(relation, hypothesis):
-    logic = relation['loop']['logic'].format(**hypothesis)
-    status = run(['z3', '-in'], input=logic, encoding='utf8', check=True, stdout=PIPE)
-    status = status.stdout.splitlines()
-    if status[0] != 'sat':
-        print(status[0], hypothesis)
-        return
+    def negative_point(self, expr):
+        logic = self.logic(expr)
+        status = run(['z3', '-in'], input=logic, encoding='utf8', stdout=PIPE)
+        status = status.stdout.splitlines()
+        if status[0] != 'sat':
+            return status[0]
 
-    point = { key: None for key in relation['head']['start'].keys() }
-    model = loads(''.join(status[1:]))
-    assert model[0].value() == 'model'
-    for var in model[1:]:
-        if var[1].value() in point:
-            value = var[-1]
-            # Convert value to integer if negative (- 1)
-            if isinstance(value, list):
-                assert value[0].value() == '-'
-                value = -value[1]
-            point[var[1].value()] = value
-
-    return point
+        point = { key: None for key in self.start.keys() }
+        model = loads(''.join(status[1:]))
+        assert model[0].value() == 'model'
+        for var in model[1:]:
+            if var[1].value() in point:
+                value = var[-1]
+                # Convert value to integer if negative (- 1)
+                if isinstance(value, list):
+                    assert value[0].value() == '-'
+                    value = -value[1]
+                point[var[1].value()] = value
+        return point
 
 
-def unscience(num):
-    return str(decimal.getcontext().create_decimal(decimal.Decimal(num)))
+    def to_hypo(self, expr):
+        return {
+            'prerel': expr,
+            'postrel': self.primer.sub('\\1p', expr),
+        }
 
 
-# def tree_classify(Classifier, X, y):
-#     def tree_to_z3(tree, symbols):
-#         if len(tree.keys()) == 0:
-#             return 'true';
-#
-#         children = list(map(lambda t: tree_to_z3(t, symbols), tree['sub'].values()))
-#         root = linsvm_to_z3(tree['clf'], symbols)
-#
-#         if len(children) == 0:
-#             return root
-#         return '(and %s (or %s))' % (root, ' '.join(children))
-#     def step(Xi, Yi):
-#         clf = Classifier()
-#         clf = clf.fit(Xi, Yi)
+class SimpleRelation(Relation):
+    def __init__(self):
+        self.start = { 'x': 1, 'y': 0 }
+        self.start_logic = '(and (= x 1) (= y 0))' # TODO: when do we need this?
+        self.asserts = [
+            '(= xp (+ x y))',
+            '(= yp (+ y 1))',
+        ]
+        self.setup()
 
-#         visualize_svm_2d(clf, Xi, Yi, 'first 10')
-#         print(Xi)
-#         print(Yi)
-#         input()
-
-#         wrong = [
-#             pred
-#             for truth, pred in zip(Yi, clf.predict(Xi))
-#             if truth != pred
-#         ]
-#         subtree = {}
-#         for clazz in wrong:
-#             Xj, Yj = [], []
-#             for x, truth, pred in zip(Xi, Yi, clf.predict(Xi)):
-#                 if pred == clazz:
-#                     Xj += [ x ]
-#                     Yj += [ truth ]
-#             subtree[clazz] = step(Xj, Yj)
-#         return {
-#             'clf': clf,
-#             'sub': subtree,
-#         }
-
-#     return step(X, y)
+    def script(self, *, x, y):
+        return {
+            'x': x + y,
+            'y': y + 1,
+        }
 
 
-def z3_int(num):
-    if num >= 0:
-        return unscience(abs(num))
-    return '(- %s)' % unscience(num)
+def tree_to_z3(clf, syms):
+    def unscience(num):
+        return str(decimal.getcontext().create_decimal(decimal.Decimal(num)))
+
+    def z3_int(num):
+        if num >= 0:
+            return unscience(abs(num))
+        return '(- %s)' % unscience(abs(num))
+
+    n_nodes = clf.tree_.node_count
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+    value = clf.tree_.value
+
+    def step(idx):
+        if children_left[idx] == children_right[idx]:
+           if value[idx][0][0] == 0 and value[idx][0][1] > 0:
+               return (True, [[]]) # found a path to the +
+           return (False, [[]])    # found a path to the -
+
+        lfound, lepxrs = step(children_left[idx])
+        rfound, rexprs = step(children_right[idx])
+
+        decision = lambda d: '({dir} {var} {threshold})'.format(**{
+            'var': syms[feature[idx]],
+            'threshold': z3_int(threshold[idx]),
+            'dir': '<=' if d == 'l' else '>',
+        })
+
+        if lfound and not rfound:
+            return (True, [ p + [decision('l')] for p in lepxrs ])
+
+        elif rfound and not lfound:
+            return (True, [ p + [decision('r')] for p in rexprs ])
+
+        elif rfound and lfound:
+            return (True, lexprs + rexprs)
+
+        return (False, None)
+
+    paths = step(0)[1]
+    conj = list(map(lambda e: '(and {})'.format(' '.join(e)), paths))
+    disj = '(or {})'.format(' '.join(conj)) if len(conj) > 1 else conj[0]
+
+    return disj
 
 
-def visualize_svm_2d(clf, X, y, title):
+def visualize_2d(clf, X, y, title):
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -157,97 +171,72 @@ def visualize_svm_2d(clf, X, y, title):
     plt.show()
 
 
-# def linsvm_to_z3(clf, syms):
-#     coeffs = clf.coef_[0]
-#     threshold = -clf.intercept_[0]
-#     notzero = lambda l: filter(lambda i: abs(i[1]) != 0, l)
-#     linavg = list(map(simpl_mult, notzero(zip(syms, coeffs))))
-#     if len(linavg) == 1:
-#         return '(> %s %s)' % (linavg[0], z3_int(threshold))
-#     else:
-#         return '(> (+ %s) %s)' % (' '.join(linavg), z3_int(threshold))
-
-
-def to_lst(obj):
-    return list(map(lambda a: a[1], sorted(obj.items(), key=lambda a: a[0])))
-
-
-def to_hyp(hyp, primed):
-    return { 'prerel': hyp, 'postrel': primed(hyp) }
-
-
-# def simpl_mult(params):
-#     sym, coeff = params
-#     if coeff == 1:
-#         return sym
-#     elif coeff == -1:
-#         return '(- %s)' % sym
-#     else:
-#         return '(* %s %s)' % (sym, z3_int(coeff))
-
-
 def loop(relation, goal):
-    syms = list(sorted(relation['head']['start'].keys()))
-    primer = re.compile('(%s)' % '|'.join(map(re.escape, syms)))
-    primed = lambda expr: primer.sub('\\1p', expr)
-    get_neg = lambda g: negative_point(relation, to_hyp(g, primed))
+    def to_lst(obj):
+        return list(map(lambda a: a[1],
+                        sorted(obj.items(), key=lambda a: a[0])))
 
     # get seed positive points
-    positive_gen = positive_points(relation)
+    positive_gen = relation.positive_points()
     positives = [ to_lst(next(positive_gen)) ]
 
     # get an initial negative point (just try the goal)
-    negatives = [ to_lst(get_neg(goal)) ]
+    negatives = [ to_lst(relation.negative_point(goal)) ]
 
     while True:
-        print('hypothesis', goal)
-        print('negatives', list(map(tuple, negatives)))
+        logger.debug('hypothesis ' + goal)
+        logger.debug('negatives ' + str(list(map(tuple, negatives))))
 
         # Gather data
         y = [ 1 for _ in positives ] + [ -1 for _ in negatives ]
         X = positives + negatives
 
-        #
-        # Old SVM way
-        #
         # Train a tree of linear separators (to get 100% accuracy)
-        # tree = tree_classify(lambda: svm.LinearSVC(C=0.1), X, y)
-        # Get the line into a z3 expression
-        # goal = tree_to_z3(tree, syms)
-        # visualize_svm_2d(tree['clf'], X, y, goal)
-        # input()
+        clf = tree.DecisionTreeClassifier()
+        clf.fit(X, y)
+
+        # Get the boxes into a z3 expressions
+        goal = tree_to_z3(clf, relation.syms)
+
+        # Make a pretty graph
+        if 'INTERACTIVE' in os.environ:
+            visualize_2d(clf, X, y, goal)
+            input()
 
         # Give line to the oracle and to see if its correct
-        counterexample = get_neg(goal)
-        if not counterexample:
-            break # no counterexamples, we're done!
+        counterexample = relation.negative_point(goal)
 
-        # check if we made progress
+        if isinstance(counterexample, str):
+            if counterexample == 'unsat':
+                return goal # no counterexamples, we're done!
+            else:
+                raise 'unknown z3 exit line: ' + counterexample
+
+        # add counterexample to negative spots
         counterexample = to_lst(counterexample)
         if counterexample in negatives:
-            print('already seen counter-example %s no progress!' % str(counterexample))
-            print('line', goal)
-            return clf
+            raise 'already seen counter-example %s no progress!' % str(counterexample)
         negatives += [ counterexample ]
 
         # add some more positive stuff
         positives += [ to_lst(next(positive_gen)) ]
 
-# clf = loop(relations['simple'], '(>= x y)')
+
+if 'DEBUG' in os.environ:
+    logger.basicConfig(level=logger.DEBUG)
+
+interpolant = loop(SimpleRelation(), '(>= x y)')
+print('success', interpolant)
 
 
-N = [(0, 0), (1, -1), (3, -2), (4, -2), (7, -3), (8, -3), (12, -4), (13, -4), (14, -4), (15, -4)]
-P = [(1, 0), (1,  1), (2,  2), (4,  3), (7,  4), (11, 5), (16,  6), (22,  7), (29,  8), (37,  9)]
-y = [ 1 for _ in range(10) ] + [ -1 for _ in range(10) ]
-X = P + N
-
-# tree = tree_classify(lambda: svm.LinearSVC(C=0.5), X, y)
-tree = tree_classify(tree.DecisionTreeClassifier, X, y)
-print(tree_to_z3(tree, ['x', 'y']))
-visualize_svm_2d(tree['clf'], X, y, 'first 10')
-
+# N = [(0, 0), (1, -1), (3, -2), (4, -2), (7, -3), (8, -3), (12, -4), (13, -4), (14, -4), (15, -4)]
+# P = [(1, 0), (1,  1), (2,  2), (4,  3), (7,  4), (11, 5), (16,  6), (22,  7), (29,  8), (37,  9)]
+# y = [ 1 for _ in range(10) ] + [ -1 for _ in range(10) ]
+# X = P + N
+# clf = tree.DecisionTreeClassifier()
+# clf.fit(X, y)
+# visualize_2d(clf, X, y, 'first 10')
+# logger.debug(tree_to_z3(clf, ['x', 'y']))
 # dot = tree.export_graphviz(clf, out_file=None, feature_names=['x', 'y'], class_names=['-', '+'], filled=True, rounded=True, special_characters=True)
 # graph = graphviz.Source(dot)
 # graph.render("iris")
-
-import ipdb; ipdb.set_trace()
